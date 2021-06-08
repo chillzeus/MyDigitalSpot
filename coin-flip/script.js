9fce896555f8c0809e8a67dded6be2a2b9016c6b
ChillZeus <fiedlerisaac1@gmail.com>
2021-06-08 13:33:13 +0000

    I guess you can't name stuff index.js

--- coin-flip/script.js
+++ coin-flip/script.js
@@ -6,7 +6,7 @@ var number = getRandNumber(2);
 
 var headsOrTails = "None";
 
-if (number === 1) {
+if (number == 1) {
 	document.getElementById("coin").innerHTML = "<h1>You flipped Heads!</h1>";
 } else {
 	document.getElementById("coin").innerHTML = "<h1>You flipped Tails!</h1>";

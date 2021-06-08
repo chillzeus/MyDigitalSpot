e6421f8619d51d7ce75804d123c7b644478005ef
ChillZeus <fiedlerisaac1@gmail.com>
2021-06-08 13:28:27 +0000

    my little unfinished coin flip app

---
+++ coin-flip/index.js
@@ -0,0 +1,13 @@
+function getRandNumber(max) {
+	return Math.floor(Math.random() * max);
+}
+
+var number = getRandNumber(2);
+
+var headsOrTails = "None";
+
+if (number === 1) {
+	document.getElementById("coin").innerHTML = "<h1>You flipped Heads!</h1>";
+} else {
+	document.getElementById("coin").innerHTML = "<h1>You flipped Tails!</h1>";
+}

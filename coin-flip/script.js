1fde802d188a3b83417be70bc51cd28c38dfb173
ChillZeus <76137066+ChillZeus@users.noreply.github.com>
2021-06-16 19:37:18 -0500

    Update script.js

--- coin-flip/script.js
+++ coin-flip/script.js
@@ -1,14 +1,15 @@
+// tells how to make rand number
 function getRandNumber(max) {
 	return Math.floor(Math.random() * max);
 }
 
-
+// creates random number
 const flipCoin = () => {
 	const number = getRandNumber(2);
-
+	
 	const headsOrTails = number === 1 ? "Heads" : "Tails";
 
-	const newHtml = `<h1>You flipped ${headsOrTails}!`;
+	const newHtml = `<h1>You flipped ${headsOrTails}!`; // ${headsOrTails} just takes from the const
 
-	document.getElementById("coinimg").innerHTML = newHtml;
+	document.getElementById("coinimg").innerHTML = newHtml; // gets the element and replaces it
 }

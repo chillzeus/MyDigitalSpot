c4b347002b83f6f340471b244bc9d2219bce2302
chillzeus <76137066+chillzeus@users.noreply.github.com>
2022-10-26 13:50:18 -0500

    A couple more changes, need to sync across devices....

--- gpa-calculator/script.js
+++ gpa-calculator/script.js
@@ -3,19 +3,21 @@ let amountOfGrades = 0;
 let grades = 0;
 
 
-function a() { grades += 4; amountOfGrades += 1; }
-function a_minus() { grades += 3.66; amountOfGrades += 1; }
-function b_plus() { grades += 3.33; amountOfGrades += 1; }
-function b() { grades += 3; amountOfGrades += 1; }
-function b_minus() { grades += 2.66; amountOfGrades += 1; }
-function c_plus() { grades += 2.33; amountOfGrades += 1; }
-function c() { grades += 2; amountOfGrades += 1; }
-function c_minus() { grades += 1.66; amountOfGrades += 1; }
-function d_plus() { grades += 1.33; amountOfGrades += 1; }
-function d() { grades += 1; amountOfGrades += 1; }
-function d_minus() { grades += 0.66; amountOfGrades += 1; }
-function f() { grades += 0; amountOfGrades += 1; }
+function a() { grades += 4; amountOfGrades += 1; calculateGPA() }
+function a_minus() { grades += 3.66; amountOfGrades += 1; calculateGPA() }
+function b_plus() { grades += 3.33; amountOfGrades += 1; calculateGPA() }
+function b() { grades += 3; amountOfGrades += 1; calculateGPA() }
+function b_minus() { grades += 2.66; amountOfGrades += 1; calculateGPA() }
+function c_plus() { grades += 2.33; amountOfGrades += 1; calculateGPA() }
+function c() { grades += 2; amountOfGrades += 1; calculateGPA() }
+function c_minus() { grades += 1.66; amountOfGrades += 1; calculateGPA() }
+function d_plus() { grades += 1.33; amountOfGrades += 1; calculateGPA() }
+function d() { grades += 1; amountOfGrades += 1; calculateGPA() }
+function d_minus() { grades += 0.66; amountOfGrades += 1; calculateGPA() }
+function f() { grades += 0; amountOfGrades += 1; calculateGPA() }
 
-function calculateGradePointAverage() {
+
+function calculateGPA() {
     gpa = grades / amountOfGrades;
+    document.getElementById("gpa-display").innerHTML = gpa;
 }

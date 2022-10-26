ad5c81ef8d8d879f1964b3364f100ae4c1430d4e
chillzeus <76137066+chillzeus@users.noreply.github.com>
2022-10-26 11:37:04 -0500

    Added functions and started basic UI for GPA calculator

--- gpa-calculator/script.js
+++ gpa-calculator/script.js
@@ -3,7 +3,18 @@ let amountOfGrades = 0;
 let grades = 0;
 
 
-function a() { grades += 4.0; amountOfGrades += 1; }
+function a() { grades += 4; amountOfGrades += 1; }
+function a_minus() { grades += 3.66; amountOfGrades += 1; }
+function b_plus() { grades += 3.33; amountOfGrades += 1; }
+function b() { grades += 3; amountOfGrades += 1; }
+function b_minus() { grades += 2.66; amountOfGrades += 1; }
+function c_plus() { grades += 2.33; amountOfGrades += 1; }
+function c() { grades += 2; amountOfGrades += 1; }
+function c_minus() { grades += 1.66; amountOfGrades += 1; }
+function d_plus() { grades += 1.33; amountOfGrades += 1; }
+function d() { grades += 1; amountOfGrades += 1; }
+function d_minus() { grades += 0.66; amountOfGrades += 1; }
+function f() { grades += 0; amountOfGrades += 1; }
 
 function calculateGradePointAverage() {
     gpa = grades / amountOfGrades;

be7aa990c2e38189f29322cdef73d06b4851c8b8
Isaac Fiedler <fiedlerisaac1@gmail.com>
2022-10-26 06:30:23 -0500

    started GPA calculator thingy

---
+++ gpa-calculator/script.js
@@ -0,0 +1,10 @@
+let gpa = 0;
+let amountOfGrades = 0;
+let grades = 0;
+
+
+function a() { grades += 4.0; amountOfGrades += 1; }
+
+function calculateGradePointAverage() {
+    gpa = grades / amountOfGrades;
+}

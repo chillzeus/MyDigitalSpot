c5469c435cede543dae87d6e183c435e5fa3ed5a
Isaac Fiedler <fiedlerisaac1@gmail.com>
2023-01-21 18:02:11 -0600

    Some data can be displayed on site, only Brawl ID

--- brawlhalla-stats/script.js
+++ brawlhalla-stats/script.js
@@ -2,23 +2,28 @@ const apikey = "R2AFVY1J6D3VNV0DFTCR"
 
 let brawlId;
 
-let steamSearchData;
+let userData;
 
 async function searchByURL(file) {
     let x = await fetch(file);
     let y = await x.json();
-    console.log(y)
+    userData = y;
+    console.log(userData);
+    display();    
 }
-
 let searchTerm = null;
 
 function search() {
     searchTerm = document.getElementById("searchBox").value;
-    console.log(searchTerm);
-    if (searchTerm.length == 17) {
-        searchByURL("https://api.brawlhalla.com/search?steamid=" + searchTerm + "&api_key=" + apikey)
-    }
-    else {
-        searchByURL("https://api.brawlhalla.com/player/" + searchTerm + "/stats?api_key=" + apikey)
-    }
+    searchByURL("https://api.brawlhalla.com/player/" + searchTerm + "/stats?api_key=" + apikey)
+}
+
+function display() {
+    let winrate = userData.wins / userData.games
+    winrate = winrate.toFixed(2)
+    winrate = winrate * 100;
+    let xpPercent = userData.xp_percentage.toFixed(2) * 100;
+    document.getElementById("name").innerHTML = "<h1>" + userData.name + "</h1>"
+    document.getElementById("winrateContainer").innerHTML = "<h3>Wins: " + userData.wins + " | Total games: " + userData.games + " | Winrate: " + winrate + "%</h3>"
+    document.getElementById("level").innerHTML = "<h3>Level: " + userData.level + " (" + xpPercent + "%) | XP: " + userData.xp; + "</h3>"
 }
